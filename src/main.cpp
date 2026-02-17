af85bc87490039bb110672e5049d776bdaea57bb
omardev29 <omaroficial365@gmail.com>
2026-02-17 22:51:07 +0100

    template ready

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include "raylib.h"
+#include <iostream>
+
+
+int main(void)
+{
+
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "raylib [core] example - basic window");
+
+
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawRectangle(300, 100, 100, 100, BLUE);
+
+
+		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+
+
+		EndDrawing();
+	}
+
+
+
+
+
+	CloseWindow();
+
+	return 0;
+}
