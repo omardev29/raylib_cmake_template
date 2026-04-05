c9a3edb33d6007815cddd63ffe5aee9ab96f9800
omardev29 <omaroficial365@gmail.com>
2026-04-05 18:29:35 +0200

    multi editor support complete

--- thirdparty/raylib-5.5/cmake/InstallConfigurations.cmake
+++ thirdparty/raylib-5.5/cmake/InstallConfigurations.cmake
@@ -8,16 +8,16 @@ install(
 
 # PKG_CONFIG_LIBS_PRIVATE is used in raylib.pc.in
 if (NOT BUILD_SHARED_LIBS)
-    include(LibraryPathToLinkerFlags)
-    set(PKG_CONFIG_LIBS_PRIVATE ${GLFW_PKG_LIBS})
-    string(REPLACE ";" " " PKG_CONFIG_LIBS_PRIVATE "${PKG_CONFIG_LIBS_PRIVATE}")
+  include(LibraryPathToLinkerFlags)
+  set(PKG_CONFIG_LIBS_PRIVATE ${GLFW_PKG_LIBS})
+  string(REPLACE ";" " " PKG_CONFIG_LIBS_PRIVATE "${PKG_CONFIG_LIBS_PRIVATE}")
 elseif (BUILD_SHARED_LIBS)
-    set(PKG_CONFIG_LIBS_EXTRA "")
+  set(PKG_CONFIG_LIBS_EXTRA "")
 endif ()
 
 join_paths(libdir_for_pc_file "\${exec_prefix}" "${CMAKE_INSTALL_LIBDIR}")
 join_paths(includedir_for_pc_file "\${prefix}" "${CMAKE_INSTALL_INCLUDEDIR}")
-configure_file(../raylib.pc.in raylib.pc @ONLY)
+#configure_file(../raylib.pc.in raylib.pc @ONLY)
 configure_file(../cmake/raylib-config-version.cmake raylib-config-version.cmake @ONLY)
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/raylib.pc DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/raylib-config-version.cmake DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/raylib")
